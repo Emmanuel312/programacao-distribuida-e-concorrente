7f77e40bb1178923dfb28d126344a44657087872
Kilnerjhow <jhow.k.132@gmail.com>
2021-11-02 13:14:16 -0300

    fix: car not picking all passengers

--- exercicio 04/exerc04.go
+++ exercicio 04/exerc04.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
 	"sync/atomic"
+	"time"
 )
+
+var count = 0
 
 type Car struct {
 	boardQueue   *sync.Cond
@@ -38,13 +42,13 @@ func load(c *Car) {
 	//signal para o passageiro
 	c.boardQueue.L.Lock()
 	c.board.Signal()
-	atomic.StoreInt32(&c.boarding, 1)
+	// atomic.StoreInt32(&c.boarding, 1)
 	for c.boarded < c.capacity {
 		c.boardQueue.Wait()
 		c.board.Signal()
 		// fmt.Printf("Total embarcado até agora: %v \n", atomic.LoadInt32(&c.boarded))
 	}
-	atomic.StoreInt32(&c.boarding, 0)
+	// atomic.StoreInt32(&c.boarding, 0)
 	fmt.Println("[E] Todos os passageiros embarcaram")
 	c.boardQueue.L.Unlock()
 }
@@ -55,10 +59,11 @@ func run(c *Car) {
 
 func unload(c *Car) {
 	c.unboardQueue.L.Lock()
-	c.unboard.Broadcast()
-	atomic.StoreInt32(&c.unboarding, 1)
+	c.unboard.Signal()
+	// atomic.StoreInt32(&c.unboarding, 1)
 	for c.boarded > 0 {
 		c.unboardQueue.Wait()
+		c.unboard.Signal()
 	}
 
 	fmt.Println("[D] Todos os passageiros desembarcaram")
@@ -69,6 +74,7 @@ func board(p *Passenger, c *Car) {
 
 	c.board.L.Lock()
 	c.boardQueue.Signal()
+	c.board.Wait()
 	//wait board
 	for atomic.LoadInt32(&c.boarded) == c.capacity {
 		fmt.Printf("[W] Passageiro %v aguardando para embarcar \n", p.id)
@@ -87,11 +93,11 @@ func unboard(p *Passenger, c *Car) {
 	c.unboard.L.Lock()
 	c.unboard.Wait()
 	//último passageiro diz para o carro que já está vazio
-	if atomic.LoadInt32(&c.boarded) == 1 {
-		c.unboardQueue.Signal()
-	}
+	// if atomic.LoadInt32(&c.boarded) == 1 {
+	// }
 	fmt.Printf("[D] Passageiro %v desembarcou\n", p.id)
 	atomic.AddInt32(&c.boarded, -1)
+	c.unboardQueue.Signal()
 	c.unboard.L.Unlock()
 }
 
@@ -108,8 +114,8 @@ func car(c *Car) {
 func passenger(id int, c *Car) {
 	p := &Passenger{id}
 
+	time.Sleep(time.Duration(rand.Intn(400)))
 	board(p, c)
-	// time.Sleep(500)
 	unboard(p, c)
 }
 
@@ -118,13 +124,14 @@ func main() {
 	var capacity int32 = 5
 	var p = 30
 
-	fmt.Println("Iniciando")
-
 	c := carFactory(capacity)
 	go car(c)
 
 	for i := 0; i < p; i++ {
 		go passenger(i, c)
 	}
+	// milli, _ := time.ParseDuration("5s")
+	// time.Sleep(milli)
 	_, _ = fmt.Scanln()
+
 }
