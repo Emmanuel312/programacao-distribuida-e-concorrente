51646e522bcf227189c74c341059ff0d65d60a1e
Luiz Albuquerque <laaj@cin.ufpe.br>
2021-12-06 00:21:18 -0300

    feat: adding app2.py

--- loadTest/app.py
+++ loadTest/app.py
@@ -1,4 +1,3 @@
-import os
 import numpy as np
 import matplotlib.pyplot as plt 
 MAX_ITERATION = 10
@@ -10,36 +9,17 @@ tcoFileTCP.close()
 tcoFileUDP.close()
 tcoFileMiddleware.close()
 
-def plota_bar_dupla_1(media_TCP,media_UDP,media_middleware):
-   grupos = 4
-  #  media_TCP = (9, 5, 4, 7)
-  #  media_UDP = (8, 6, 5, 3)
-  #  media_middleware = (7, 1, 2, 5)
-   fig, ax = plt.subplots()
-   indice = np.arange(grupos)
-   bar_larg = 0.2
-   transp = 0.7
-   plt.bar(indice, media_TCP, bar_larg, alpha=transp, color="blue", label='TCP')
-   plt.bar(indice + bar_larg, media_UDP, bar_larg, alpha=transp, color='green', label='UDP')
-   plt.bar(indice + 2*bar_larg, media_middleware, bar_larg, alpha=transp, color='red', label='middleware')
 
-   plt.xlabel('Número de clientes') 
-   plt.ylabel('Tempo médio de respostas (ms)') 
-   plt.title(f'N = {MAX_ITERATION}') 
-   plt.xticks(indice + bar_larg, ('1', '2', '5', '10')) 
-   plt.legend() 
-   plt.tight_layout() 
-   plt.show()
 
 def createCommand(command, client):
   model = command
-  command = "SAVE_INFO=TRUE " + command  
   for i in range(client-1):
-    command += " & " + model
+    if(i==client-2):
+      command += " & SAVE_INFO=TRUE " + model
+    else:
+      command += " & " + model
   return command
 
-# command = createCommand(f"""MAX_ITERATION={MAX_ITERATION} node ./{type}/index.js""",10)
-# print(command)
 
 meanTCPArray = []
 meanUDPArray = []
@@ -57,21 +37,9 @@ for type in ['udp','tcp', 'middleware']:
 
     sh.write(command + "\n")
  
-    # os.system(command)
-    # file = open(f"./data/{type}.txt", "r")
-    # arrayTimes = np.fromstring(file.read(), dtype=int, sep=',')
-    # meanTime = arrayTimes.mean()
-    # print(f"Média {type}: {meanTime} ms")
-    # print(f"Desvio padrão {type}: {arrayTimes.std()}")
-    # if(type=='tcp'):
-    #   meanTCPArray.append(meanTime)
-    # elif(type=='udp'):
-    #   meanUDPArray.append(meanTime)
-    # else:
-    #   meanMiddlewareArray.append(meanTime)
-    # file.close()
+   
     
-# plota_bar_dupla_1(meanTCPArray,meanUDPArray,meanMiddlewareArray)
+
 
 
 
---
+++ loadTest/app2.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import numpy as np
+MAX_ITERATION = 1000
+def plota_bar_dupla_1(media_TCP,media_UDP,media_middleware):
+   global MAX_ITERATION
+   grupos = 4
+  #  media_TCP = (9, 5, 4, 7)
+  #  media_UDP = (8, 6, 5, 3)
+  #  media_middleware = (7, 1, 2, 5)
+   fig, ax = plt.subplots()
+   indice = np.arange(grupos)
+   bar_larg = 0.2
+   transp = 0.7
+   plt.bar(indice, media_TCP, bar_larg, alpha=transp, color="blue", label='TCP')
+   plt.bar(indice + bar_larg, media_UDP, bar_larg, alpha=transp, color='green', label='UDP')
+   plt.bar(indice + 2*bar_larg, media_middleware, bar_larg, alpha=transp, color='red', label='middleware')
+
+   plt.xlabel('Número de clientes') 
+   plt.ylabel('Tempo médio de respostas (ms)') 
+   plt.title(f'N = {MAX_ITERATION}') 
+   plt.xticks(indice + bar_larg, ('1', '2', '5', '10')) 
+   plt.legend() 
+   plt.tight_layout() 
+   plt.show()
+
+meanTCPArrayByClient = []
+meanUDPArrayByClient = []
+meanMiddlewareArrayByClient = []
+
+
+for type in ['udp','tcp', 'middleware']:
+    print("#######################################################")
+    print(f"\t\t\tAnalisando {type}:")
+    file = open(f"./data/{type}.txt", "r")
+    arrayTextFile = file.read().split('\n')
+    for timesByClient,i in zip(arrayTextFile,[1,2,5,10]):
+        print(f"{i} cliente(s) ativo(s)")
+        if timesByClient  != '':
+            arrayTimesByClient = np.fromstring(timesByClient, dtype=int, sep=',')
+            meanTime = arrayTimesByClient.mean()
+            print(f" -> Média {type}: {meanTime} ms")
+            print(f" -> Desvio padrão {type}: {arrayTimesByClient.std()}")
+            if(type=='tcp'):
+                meanTCPArrayByClient.append(meanTime)
+            elif(type=='udp'):
+                meanUDPArrayByClient.append(meanTime)
+            else:
+                meanMiddlewareArrayByClient.append(meanTime)
+    
+plota_bar_dupla_1(meanTCPArrayByClient,meanUDPArrayByClient,meanMiddlewareArrayByClient)
